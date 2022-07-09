2911872d32b3471b9e5a32216e8a0410ea57f1a8
s14t284 <rikeda71@gmail.com>
2022-07-09 17:42:39 +0900

    :sparkles: add login page and profile page

---
+++ lib/ui/pages/profile/components/profile_page_body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/domain/auth/auth_state_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ProfilePageBody extends HookConsumerWidget {
+  const ProfilePageBody({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return _buildContents(context, ref);
+  }
+
+  Widget _buildContents(BuildContext context, WidgetRef ref) {
+    final profile = ref.watch(authStateNotifier.select((value) => value));
+
+    return Scaffold(
+      key: const Key('profile'),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('プロフィール'),
+          Text('name: ${profile.displayName}'),
+          Text(profile.email),
+          Text(profile.phoneNumber),
+          Text(profile.photoUrl)
+        ],
+      )),
+    );
+  }
+}
